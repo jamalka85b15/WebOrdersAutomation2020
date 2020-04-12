cee558c31151407115399922e0e3e2b0a0bc4d52
jamalka85b15 <crutchfield.nur@gmail.com>
2020-04-12 15:52:06 -0500

    PageClasses

--- src/main/java/com/weborders/pages/AbstractBasePage.java
+++ src/main/java/com/weborders/pages/AbstractBasePage.java
@@ -2,10 +2,12 @@ package com.weborders.pages;
 
 import com.sun.org.apache.xalan.internal.xsltc.dom.AdaptiveResultTreeImpl;
 import com.weborders.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 //it meant to be extended
@@ -22,6 +24,10 @@ public abstract class AbstractBasePage {
 
     public AbstractBasePage(){
         PageFactory.initElements(Driver.getDriver(), this);
+    }
+    public void navigateTo(String component){
+        String locator="//a[text()='"+component+"']";
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator))).click();
     }
 
 }
